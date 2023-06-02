6d5562ed4f963029e6c6e7b2650c0da43f8269c8
TumanovaAnna <118038015+TumanovaAnna@users.noreply.github.com>
2023-06-02 12:28:32 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,36 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <vector>
+#include <iostream>
+class Tree {
+ private:
+    std::vector<Tree*> root;
+    char smb;
+    explicit Tree(char n1) : smb(n1) {}
+    void createNode(const std::vector<char>& vec) {
+        for (int i = 0; i < vec.size(); i++) {
+            std::vector<char> temp = vec;
+            root.push_back(new Tree(temp[i]));
+            temp.erase(temp.begin() + i);
+            root[i]->createNode(temp);
+        }
+    }
 
+public:
+    explicit Tree(const std::vector<char>& vec) {
+        smb = '*';
+        createNode(vec);
+    }
+
+    int getSize() const {
+        return root.size();
+    }
+    char getSymb() const {
+        return smb;
+    }
+    Tree& operator[](int n) const {
+        return *root[n];
+    }
+};
 #endif  // INCLUDE_TREE_H_
